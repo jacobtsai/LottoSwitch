9db5a7100b30467f934171c2753b91f426925137
Jacob <Jacobtsai8888@gmail.com>
2026-03-23 09:25:28 +0800

    feat: auto-select text inside input fields on focus for better UX

--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// 全域監聽 Focus 事件，當任何 input 被選中時自動全選文字
+window.addEventListener('focusin', (e) => {
+  if (e.target && e.target.tagName === 'INPUT' && (e.target.type === 'text' || e.target.type === 'number')) {
+    e.target.select()
+  }
+})
+
 createApp(App).mount('#app')
